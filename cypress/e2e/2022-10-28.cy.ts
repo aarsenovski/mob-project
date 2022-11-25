a1d4e4ef3feaea4c7e461e98655f378d9f984fde
Tim Lolkema <t.lolkema@labdigital.nl>
2022-11-25 13:19:45 +0100

    feat: cleanup mob repo and write summaries

---
+++ cypress/e2e/2022-10-28.cy.ts
@@ -0,0 +1,24 @@
+import { previewList } from '../support/preview'
+
+previewList.forEach((preview) => {
+  describe(`Cart  ${preview}`, () => {
+    beforeEach(() => {
+      cy.viewport(preview)
+    })
+    it('should log out dynamic values', () => {
+      // const a and b are different ways how you can concatenate strings
+      // we use the dynamic value for `preview` here which will be the used preset
+      const a = `Cart  ${preview}`
+      const b = 'Cart' + ' ' + preview
+
+      cy.log(a, b)
+    })
+
+    it('should use custom command', () => {
+      cy.visit('http://www.google.com')
+
+      // we don't need to import custom commands but can use them straight away with cy.methodName()
+      cy.search('Frasers')
+    })
+  })
+})
